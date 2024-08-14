3216b1792089c1b53f98f301318cabad29574c1b
nayanaiyer <117653547+nayanaiyer@users.noreply.github.com>
2024-08-14 11:18:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 REPO_NAME = "Books Recommendation System Using Machine Learning and Collaborative Filtering"
 AUTHOR_USER_NAME = "nayanaiyer"
 SRC_REPO = "src"
-LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
+LIST_OF_REQUIREMENTS = ['streamlit', 'numpy','scikit-learn', 'pandas', 'pickle']
 
 
 setup(
